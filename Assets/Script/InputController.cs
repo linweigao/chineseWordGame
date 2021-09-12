db917b4cb2b86a311c34e78b365eee3d68e98f36
Linwei Gao <jogao1983@gmail.com>
2021-09-11 23:08:48 -0700

    help & map button

--- Assets/Script/InputController.cs
+++ Assets/Script/InputController.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class InputController : MonoBehaviour
 {
     public TMP_InputField input;
+    public Button helpBtn;
+    public Button mapBtn;
 
     private GameController gameController;
 
     void Awake()
     {
-        input.onSubmit.AddListener(InputSubmitted);
+        this.input.onSubmit.AddListener(InputSubmitted);
+        this.helpBtn.onClick.AddListener(HelpClicked);
+        this.mapBtn.onClick.AddListener(MapClicked);
         this.gameController = this.GetComponent<GameController>();
     }
 
@@ -25,6 +30,16 @@ public class InputController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void HelpClicked()
+    {
+        this.gameController.HandleInput("?");
+    }
+
+    private void MapClicked()
+    {
+        this.gameController.HandleInput("#");
     }
 
     private void InputSubmitted(string data)
